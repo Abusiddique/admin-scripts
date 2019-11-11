d8949254b1de4d112a0e08f5ea3fe122010a11d2
Abusiddique <57164954+Abusiddique@users.noreply.github.com>
2019-11-11 22:43:03 +0530

    Added file to clean temp directory
    Script developed using subprocess package

--- clear_temp_files.py
+++ clear_temp_files.py
@@ -1,19 +1,40 @@
+"""
+Script to clean all temp files in windows temp directory
+to improve performance of system
+
+This script uses subprocess package
+"""
+
 import os, subprocess
 
-del_dir = r'c:\\windows\\temp'
+def clean_directory(del_dir):
+	'''
+	Function to delete files in given directory along with sub directories
 
-# command to delete the files in temp directory
+	Input:
+		directory path to be delted
+	Output:
+		return_code - 0(success), 1(failure)
+	'''
+	
+	# command to delete the files in temp directory clean files in the directory but do not delete subdirectories
+	#pObj = subprocess.Popen('del /S /Q /F %s\\*.*' % del_dir, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
 
+	# command to delete the files in temp directory clean files in the directory and delete subdirectories and parent directory
+	pObj = subprocess.Popen('rmdir /S /Q %s' % del_dir, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
 
-#pObj = subprocess.Popen('del /S /Q /F %s\\*.*' % del_dir, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
-pObj = subprocess.Popen('rmdir /S /Q %s' % del_dir, shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
-# recreate the deleted parent dir in case it get deleted
-os.makedirs(del_dir)
+	# recreate the deleted parent dir in case it get deleted
+	os.makedirs(del_dir)
 
-rTup = pObj.communicate()
-rCod = pObj.returncode
+	return_value = pObj.communicate()
+	return_code = pObj.returncode
 
-if rCod == 0:
-    print 'Success: Cleaned Windows Temp Folder'
-else:
-    print 'Fail: Unable to Clean Windows Temp Folder'
+try:
+	return_code = clean_directory(r'c:\\windows\\temp')
+
+	if return_code == 0:
+	    print('Success: Cleaned Windows Temp Folder')
+	else:
+	    print('Fail: Unable to Clean Windows Temp Folder')
+except Exception as error:
+	print("Exception occured: " + str(error))
